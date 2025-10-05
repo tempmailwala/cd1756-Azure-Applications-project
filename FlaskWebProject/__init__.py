3184219459333b00dd35e0d6733201e00f70bb16
tempmailwala <temp.mail.wala@gmail.com>
2025-10-05 20:59:29 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,26 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# This logs to console (Azure Log Stream) and file (optional)
+app.logger.setLevel(logging.INFO)
+
+# File handler (optional)
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'
+))
+app.logger.addHandler(file_handler)
+
+# Stream handler for Azure
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
+
+app.logger.info("Flask CMS app started successfully")
+
+# Flask extensions
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
